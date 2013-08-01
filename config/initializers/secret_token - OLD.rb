518cbe557265bb2db1a165dbc0bbf2fdddac01b3
Vinnie Vin <vincent1897@gmail.com>
2013-08-01 01:52:32 -0400

    Initial commit

---
+++ config/initializers/secret_token - OLD.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '36c59c1db930e87a6fc44c32d64b96ae7e43806fbf45b8541d94dc75010d47cceaf4db38bc4319da67257d870e04db06f55c366c818687b791aade3661740d95'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,27 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+
+#SampleApp::Application.config.secret_token = '36c59c1db930e87a6fc44c32d64b96ae7e43806fbf45b8541d94dc75010d47cceaf4db38bc4319da67257d870e04db06f55c366c818687b791aade3661740d95'
+
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+SampleApp::Application.config.secret_key_base = secure_token
+
